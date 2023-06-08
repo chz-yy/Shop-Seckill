a72ada87d98bfba43cd53380084c27edf632aabd
xiaoliu <liugang@wolfcode.cn>
2023-06-08 15:51:44 +0800

    分布式锁-Redisson客户端加锁

--- shop-parent/shop-provider/seckill-server/src/main/java/cn/wolfcode/service/impl/OrderInfoSeviceImpl.java
+++ shop-parent/shop-provider/seckill-server/src/main/java/cn/wolfcode/service/impl/OrderInfoSeviceImpl.java
@@ -12,13 +12,13 @@ import cn.wolfcode.service.IOrderInfoService;
 import cn.wolfcode.service.ISeckillProductService;
 import cn.wolfcode.util.IdGenerateUtil;
 import cn.wolfcode.web.msg.SeckillCodeMsg;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -31,11 +31,7 @@ public class OrderInfoSeviceImpl implements IOrderInfoService {
     @Autowired
     private OrderInfoMapper orderInfoMapper;
     @Autowired
-    private StringRedisTemplate redisTemplate;
-
-    // 调度线程池
-    private final ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(5);
-
+    private RedissonClient redisson;
     @Autowired
     private PayLogMapper payLogMapper;
     @Autowired
@@ -50,39 +46,12 @@ public class OrderInfoSeviceImpl implements IOrderInfoService {
     @Override
     public String doSeckill(UserInfo userInfo, SeckillProductVo vo) {
         // 再次判断库存是否足够
-        // setIfAbsent == Redis 中的 SETNX 方法
-        // SETNX：如果设置的 key 在 redis 中已经存在，就返回 false，如果不存在就返回 true
-        // 如果当前线程执行结果为 true，我就认为它抢到了锁，如果为 false 我就认为争抢锁失败，直接提示网络繁忙稍后再试
         final String key = "seckill:product:lock:" + vo.getId();
-        // 为了避免锁被别人释放，因此引入一个唯一标识，用来表示当前线程，释放锁时需要进行校验这个锁是否是自己的，如果是才能释放
-        final String threadId = IdGenerateUtil.get().nextId() + "";
+        // 获取锁
+        RLock lock = redisson.getLock(key);
         try {
             // 加锁
-            // 设置了超时时间，避免获取到锁后，进程被关闭，无法释放锁导致死锁问题
-            // TODO: Spring Data 所提供的 setIfAbsent(key, value, timeout, unit) 存在并发问题
-            // TODO: 原因是该方法是通过先后调用 SET + EXPIRE 指令实现的如果 key 不存在就设置，并设置超时时间的功能
-            // TODO: 因为是有多个指令组成的，此时如果其中一个指令执行成功，另一个失败则还是可能出现死锁问题
-            // TODO: 真正通过 SETNX + EXPIRE 实现的方法需要通过管道命令 或 LUA 脚本实现批处理命令才可以避免加锁成功但是设置超时时间失败的问题
-            long time = 5L;
-            Boolean ret = redisTemplate.opsForValue().setIfAbsent(key, threadId, time, TimeUnit.SECONDS);
-            if (ret == null || !ret) {
-                System.err.println(Thread.currentThread().getName() + "------------------加锁失败------------------");
-                // 如果加锁失败，就抛出异常
-                throw new BusinessException(SeckillCodeMsg.SECKILL_BUSY);
-            }
-
-            // 发布一个定时任务，在 key 快要过期的时候检查该 key 是否存在，如果已经不存在，说明当前业务已经执行完成
-            // 如果还存在，说明业务还没有执行完成，如果还没有执行完成，我们可以重新为该 key 增加有效时间
-            // 看门狗机制 == Redisson 中的 WatchDog
-            scheduledThreadPoolExecutor.schedule(() -> {
-                // 该任务会在 time-1 秒后执行
-                // 去检查当前线程加的 key 是否还存在
-                if (threadId.equals(redisTemplate.opsForValue().get(key))) {
-                    // 需要判断是否是当前线程加的锁，是当前线程加的且还没有执行完才需要续时长
-                    redisTemplate.expire(key, time, TimeUnit.SECONDS);
-                }
-            }, time - 1, TimeUnit.SECONDS);
-
+            lock.lock(5, TimeUnit.SECONDS);
             // 如果加锁成功，就扣减库存
             SeckillProduct sp = seckillProductService.findById(vo.getId());
             if (sp.getStockCount() <= 0) {
@@ -93,10 +62,7 @@ public class OrderInfoSeviceImpl implements IOrderInfoService {
             seckillProductService.decrStockCount(vo.getId(), vo.getTime());
         } finally {
             // 释放锁
-            // 释放锁时，只能释放自己的锁，不能释放别人的锁
-            if (threadId.equals(redisTemplate.opsForValue().get(key))) {
-                redisTemplate.delete(key);
-            }
+            lock.unlock();
         }
         // 2. 创建秒杀订单并保存
         OrderInfo orderInfo = this.buildOrderInfo(userInfo, vo);
